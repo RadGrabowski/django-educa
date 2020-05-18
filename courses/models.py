90010d6bd4652dc072fc9c0c5c016194409cc159
RadGrabowski <radek18g@gmail.com>
2020-05-18 16:34:51 +0200

    added custom model fields to order modules and content

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -34,9 +35,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
+
+    class Meta:
+        ordering = ['order']
 
 
 class Content(models.Model):
@@ -45,6 +50,10 @@ class Content(models.Model):
                                      limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
@@ -52,25 +61,25 @@ class ItemBase(models.Model):
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         abstract = True
-        
+
     def __str__(self):
         return self.title
-    
+
 
 class Text(ItemBase):
     content = models.TextField()
-    
+
 
 class File(ItemBase):
     file = models.FileField(upload_to='files')
-    
+
 
 class Image(ItemBase):
     file = models.FileField(upload_to='images')
-    
+
 
 class Video(ItemBase):
     url = models.URLField()
